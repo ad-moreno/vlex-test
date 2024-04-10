dc219fe263c456c82c03b10148020859472bb89e
Adrian Moreno <ad.moreno@am-im.com>
2024-04-10 18:25:57 +0200

    Initial scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white font-sans text-gray-900">Hello</div>
+  );
+};
+
+export default App;
